328fb1a05956638b9a7b9ffabdfaeec78c8a62fe
david-develop <924@holbertonschool.com>
2020-02-07 14:53:20 -0500

    Welcome message

--- color_test.scala
+++ color_test.scala
@@ -14,8 +14,8 @@ class circle {
 			print(Console.BLUE + form + Console.RESET)
 		} else if (color == "G") {
 			print(Console.GREEN + form + Console.RESET)
-		} else if (color == "C") {
-			print(Console.CYAN + form + Console.RESET)
+		} else if (color == "W") {
+			print(Console.WHITE + form + Console.RESET)
 		} else if (color == "M") {
 			print(Console.MAGENTA + form + Console.RESET)
 		}
@@ -28,10 +28,23 @@ object Main {
 		var chip_bot:Array[circle]=new Array[circle](4)
 		var chip_player:Array[circle]=new Array[circle](4)
 
+		// Set random pattern
 		for (i <- 0 to 3) {
 			chip_bot(i) = new circle()
-	   		chip_bot(i).print_crl(chip_bot(i).color, chip_bot(i).form)
+	   		//chip_bot(i).print_crl(chip_bot(i).color, chip_bot(i).form)
 		}
+		// Welcome message
+		val form = "\u2B24   "
+		println("Welcome to mastermind of Stalion Team, BOG-Cohort-9")
+		println("Please select 4 color for you chips\nType the corresponding letter UPPERCASE")
+		println("Yellow      (Y) -> " + Console.YELLOW + form + Console.RESET)
+		println("Red         (R) -> " + Console.RED + form + Console.RESET)
+		println("Magenta     (M) -> " + Console.MAGENTA + form + Console.RESET)
+		println("Blue        (B) -> " + Console.BLUE + form + Console.RESET)
+		println("Green       (G) -> " + Console.GREEN + form + Console.RESET)
+		println("White       (C) -> " + Console.WHITE + form + Console.RESET)
+		println("\nYOU HAVE 10 OPORTUNITIES!\nGOOD LUCK!")
+
 	   	println("\n_____________________________\n")
 
 		for (i <- 0 to 3) {
@@ -40,7 +53,7 @@ object Main {
 			chip_player(i).color = readLine(msn_str)
 		}
 
-		println("\n_____________________________\n")
+		println("_____________________________\n")
 
 		for (i <- 0 to 3) {
 	   		chip_player(i).print_crl(chip_player(i).color, chip_player(i).form)
